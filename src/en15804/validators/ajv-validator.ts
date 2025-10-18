07c4596478bc5f190fff5ea1ac84d39b13dbf5d3
sylviesuite <stevendscott9@gmail.com>
2025-10-17 23:36:17 -0400

    chore: normalization polish

--- src/en15804/validators/ajv-validator.ts
+++ src/en15804/validators/ajv-validator.ts
@@ -2,7 +2,8 @@ import Ajv from "ajv";
 import addFormats from "ajv-formats";
 import schema from "../schemas/en15804.schema.json" assert { type: "json" };
 
-const ajv = addFormats(new Ajv({ allErrors: true, strict: true }));
+const ajv = new Ajv({ allErrors: true });
+addFormats(ajv as any);
 const validate = ajv.compile(schema);
 
 export function validateSchema(payload: unknown) {
